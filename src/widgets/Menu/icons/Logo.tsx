e48c99beed22bd7790e961cd12ec4e31e9f4eefd
Snaddyvitch-Dispenser <conor.howland@outlook.com>
2021-03-02 15:19:42 +0000

    Does it work?

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -1,21 +1,13 @@
 import React from "react";
-import Svg from "../../../components/Svg/Svg";
-import { SvgProps } from "../../../components/Svg/types";
+import { SvgProps } from "../../../components/Svg";
 
 interface LogoProps extends SvgProps {
   isDark: boolean;
 }
 
-const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
-  const textColor = isDark ? "#FFFFFF" : "#000000";
+const Logo: React.FC<LogoProps> = () => {
   return (
-    <Svg viewBox="0 0 205 68" {...props}>
-      <image
-        width="205"
-        height="68"
-        href={isDark ? "/images/egg/LogoTextNewDark.png" : "/images/egg/LogoTextNewWhite.png"}
-      />
-    </Svg>
+    <img alt="CubFinance" src="/images/cub/header_logo_wide.png" />
   );
 };
 
